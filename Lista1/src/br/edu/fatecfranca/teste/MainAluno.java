c2c1732ea244addcbea0fad0446b20ec39dd5a2f
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-08-19 12:25:54 -0300

    teste

--- Lista1/src/br/edu/fatecfranca/teste/MainAluno.java
+++ Lista1/src/br/edu/fatecfranca/teste/MainAluno.java
@@ -4,7 +4,7 @@ import br.edu.fatecfranca.exe1.Aluno;
 
 public class MainAluno {
     public static void main(String[] args) {
-        Aluno obj1 = new Aluno(1, 25, 8.3f, 9.1f, "Lucas");
+        Aluno obj1 = new Aluno(1, 28, 8.3f, 9.1f, "Lucas");
 
         System.out.println(obj1.toString());
     }
