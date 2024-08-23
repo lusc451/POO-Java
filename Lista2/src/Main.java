994fd6883ebcf9fdb80b7f65b358d7fd6a073c9e
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-08-23 19:58:39 -0300

    encapsulamento

--- Lista2/src/Main.java
+++ Lista2/src/Main.java
@@ -1,15 +1,19 @@
+import br.edu.fatecfranca.exe0.Compra;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Compra obj1 = new Compra(200, 2, "a");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Compra obj2 = new Compra();
+        obj2.setQtd(-10);
+        obj2.setValor(-200);
+        obj2.setSituacao("Bugada");
+
+        Compra obj3 = new Compra();
+        obj3.setQtd(10);
+        obj3.setValor(10);
+        obj3.setSituacao("Aprovado");
     }
 }
--- Lista2/src/br/edu/fatecfranca/exe0/Compra.java
+++ Lista2/src/br/edu/fatecfranca/exe0/Compra.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+package br.edu.fatecfranca.exe0;
 
 public class Compra {
+    private float valor;
+    private int qtd;
+    private String situacao;
+
+    public Compra() {
+    }
+
+    public Compra(float valor, int qtd, String situacao) {
+        this.valor = valor;
+        this.qtd = qtd;
+        this.situacao = situacao;
+    }
 }
