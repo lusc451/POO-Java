06935d85c0bb8204af73ba2d5e323ed4d7ebe06a
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-08-23 20:03:25 -0300

    encapsulamento

--- Lista2/src/br/edu/fatecfranca/exe0/Compra.java
+++ Lista2/src/br/edu/fatecfranca/exe0/Compra.java
@@ -13,4 +13,24 @@ public class Compra {
         this.qtd = qtd;
         this.situacao = situacao;
     }
+
+    public void setValor(float valor) {
+        if(valor >= 0){
+            this.valor = valor;
+        }
+        else {
+            System.out.println("Valor inválido");
+            this.valor = 0;
+        }
+    }
+
+    public void setQtd(int qtd) {
+        if(qtd >= 0){
+            this.qtd = qtd;
+        }
+        else {
+            System.out.println("Quantidade inválida");
+            this.qtd = 0;
+        }
+    }
 }
