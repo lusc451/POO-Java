2755934bd4af56227c7748564b09be074dc31439
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-08-31 08:18:13 -0300

    Lista3

--- Lista3/src/br/edu/lista3/teste/Main.java
+++ Lista3/src/br/edu/lista3/teste/Main.java
@@ -7,13 +7,24 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Product prod1 = new Product(1, "Bola de Basquete", 400f);
-        Product prod2 = new Product(2, "Bola de Futebol", 300f);
-        Product prod3 = new Product(3, "Bola de Vôlei", 200f);
+        Product prod1 = new Product(1, "Bola de Basquete", 400);
+        Product prod2 = new Product(2, "Bola de Futebol", 300);
+        Product prod3 = new Product(3, "Bola de Volei", 200);
 
-        ShoppingCart cart1 = new ShoppingCart(1, new Date(), "Franca", "Pix", 0f);
+        ShoppingCart cart1 = new ShoppingCart(100, new Date(), "Franca",
+                "Pix", 0);
         cart1.addCarItem(10, 2, prod1);
-        cart1.addCarItem(12, 5, prod2);
+        cart1.addCarItem(12, 5, prod1);
         cart1.calculateTotalPrice();
+        System.out.println(cart1);
+
+        ShoppingCart cart2 = new ShoppingCart(101, new Date(), "Ipuã",
+                "CC", 0);
+        cart2.addCarItem(11, 4, prod1);
+        cart2.addCarItem(13, 10, prod3);
+        cart2.calculateTotalPrice();
+        System.out.println(cart2);
+
+
     }
 }
