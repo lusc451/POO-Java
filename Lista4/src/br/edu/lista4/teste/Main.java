c60dd62f189348580be5a00d266bc0d4425d14bb
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-09-10 15:49:31 -0300

    Lista4

---
+++ Lista4/src/br/edu/lista4/teste/Main.java
@@ -0,0 +1,10 @@
+package br.edu.lista4.teste;
+import br.edu.lista4.exe0.Assistente;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Assistente assistente1 = new Assistente("Matheus", "Franca", "123.456.789-3", 3000, 4);
+        System.out.println(assistente1);
+    }
+}
