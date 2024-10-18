4b0bb000b28a121ebfb3cf0f1dcb634a56166d2a
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-10-17 22:03:16 -0300

    ApiBD

--- ApiBD/src/main/java/br/edu/fatecfranca/apibd/controller/UsuarioController.java
+++ ApiBD/src/main/java/br/edu/fatecfranca/apibd/controller/UsuarioController.java
@@ -1,9 +1,14 @@
 package br.edu.fatecfranca.apibd.controller;
 
+import br.edu.fatecfranca.apibd.model.Usuario;
 import br.edu.fatecfranca.apibd.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/usuario")
@@ -11,4 +16,17 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listarTodos(){
+        return new ResponseEntity<>(usuarioService.listaTodos(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscaPorId(@PathVariable Long id){
+        Optional<Usuario> usuario = usuarioService.buscaPorID(id);
+        return
+                usuario.isPresent() ?
+                        new ResponseEntity<>(usuario.get(), HttpStatus.OK) :
+                        new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
