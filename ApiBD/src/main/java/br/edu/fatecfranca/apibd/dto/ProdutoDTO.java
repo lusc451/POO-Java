011a43946cc9321070f56e515b305b076134e696
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-11-13 19:56:36 -0300

    ApiBD - Produto

---
+++ ApiBD/src/main/java/br/edu/fatecfranca/apibd/dto/ProdutoDTO.java
@@ -0,0 +1,12 @@
+package br.edu.fatecfranca.apibd.dto;
+
+
+import lombok.Data;
+
+@Data
+public class ProdutoDTO {
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Double preco;
+}
